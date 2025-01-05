735dea2d2350f1860a9799d8b18a5f26813a7f85
문준일 <answnsdlfwkd@naver.com>
2025-01-06 02:02:48 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/example/Tenpin/TenpinApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tenpin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenpinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenpinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Tenpin/TenpinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tenpin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenpinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
