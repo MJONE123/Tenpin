ce61d99210f87ea031fbd8d471d0fb1d5341df39
문준일 <answnsdlfwkd@naver.com>
2025-01-07 14:34:25 +0900

    feat: swagger 의존성 추가

--- src/main/java/com/example/Tenpin/controller/ExampleController.java
+++ src/main/java/com/example/Tenpin/controller/ExampleController.java
@@ -1,4 +1,14 @@
 package com.example.Tenpin.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ExampleController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "안녕@@@@@@@@@@@@";
+    }
+
+
 }
