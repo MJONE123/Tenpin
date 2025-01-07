477178dbf8b354becae4948e411ea77850c89718
문준일 <answnsdlfwkd@naver.com>
2025-01-07 17:46:35 +0900

    fix: 예시 컨트롤러 어노테이션 추가

--- src/main/java/com/example/Tenpin/controller/ExampleController.java
+++ src/main/java/com/example/Tenpin/controller/ExampleController.java
@@ -1,11 +1,18 @@
 package com.example.Tenpin.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/example")
+@Tag(name = "예시 API", description = "예시를 위한 API입니다")
 public class ExampleController {
+
     @GetMapping("/hello")
+    @Operation(summary = "안녕", description = "안녕 api입니다")
     public String hello() {
         return "안녕@@@@@@@@@@@@";
     }
